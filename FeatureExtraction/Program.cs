914ae00ec59676d95ddbd0853be2c335932e6b8a
Zhang Yang <dimlight1998@gmail.com>
2018-05-19 12:28:10 +0800

    Add solution.

---
+++ FeatureExtraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeatureExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
