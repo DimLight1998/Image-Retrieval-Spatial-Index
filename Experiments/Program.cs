e931c631c25fff6e6bba71d0079af63202daa978
Zhang Yang <dimlight1998@gmail.com>
2018-05-27 23:53:55 +0800

    Partial commit.

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -1,60 +1,71 @@
 ﻿using System;
-using System.CodeDom;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using SpatialIndex.RTree;
 
 namespace Experiments
 {
-    internal class Program
+    internal static class Program
     {
-        private const int TopK = 8;
-        private const int FeatureDimension = 12;
-        private const string DataPath = @"G:\Workspace\DS&Alg-Project1-Release\data";
-        private const string ImageFileName = "imagelist.txt";
-        private const string FeatureFileName = "rgbhist4.txt";
-        private const int RTreeMaxEntry = 30;
-        private const int RTreeMinEntry = 12;
+        private const int NumImages = 5613;
+        private const string ImageFilePath = @"G:\Workspace\DS&Alg-Project1-Release\data\imagelist.txt";
+
+        private static readonly Dictionary<int, Tuple<string, int>> StrategyMap =
+            new Dictionary<int, Tuple<string, int>>
+            {
+                {1, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\color_moment.txt", 9)},
+                {2, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\hslbin2.txt", 6)},
+                {3, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\hslbin3.txt", 9)},
+                {4, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\hslbin4.txt", 12)},
+                {5, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\hslbin5.txt", 15)},
+                {6, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\hslbin6.txt", 18)},
+                {7, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\rgbbin2.txt", 6)},
+                {8, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\rgbbin3.txt", 9)},
+                {9, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\rgbbin4.txt", 12)},
+                {10, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\rgbbin5.txt", 15)},
+                {11, new Tuple<string, int>(@"G:\Workspace\DS&Alg-Project1-Release\data\rgbbin6.txt", 18)}
+            };
+
+        private static void Shuffle<T>(this IList<T> list)
+        {
+            var rng = new Random(DateTime.Now.Millisecond);
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        private static int GetNumDiskAccess(int rtreeSize, int strategy, int minEntry, int maxEntry)
+        {
+            var featurePath = StrategyMap[strategy].Item1;
+            var featureDim = StrategyMap[strategy].Item2;
+
+            var nums = Enumerable.Range(0, NumImages).ToList();
+            nums.Shuffle();
+
+            var tree = new RTree<string>(maxEntry, minEntry);
+
+            var featureLines = File.ReadAllLines(featurePath);
+            var points = featureLines.Select(s =>
+            {
+                var slices = s.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                return new Point(featureDim, slices.Select(float.Parse).ToList());
+            }).ToList();
+            var imageNames = File.ReadAllLines(ImageFilePath);
+
+            for (var i = 0; i < rtreeSize; i++)
+                tree.AddRecord(new Rectangle(featureDim, points[nums[i]], points[nums[i]]), imageNames[nums[i]]);
+        }
+
 
         public static void Main(string[] args)
         {
-            var imageList = File.ReadAllLines(Path.Combine(DataPath, ImageFileName));
-            var featureList = File.ReadAllLines(Path.Combine(DataPath, FeatureFileName));
-
-            Debug.Assert(imageList.Length == featureList.Length);
-
-            var rTree = new RTree<string>(RTreeMaxEntry, RTreeMinEntry);
-            for (var i = 0; i < imageList.Length; i++)
-            {
-                var feature = featureList[i];
-                var name = imageList[i];
-                var slices = feature.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-                var featureArr = (from slice in slices select float.Parse(slice)).ToList();
-                var point = new Point(FeatureDimension, featureArr);
-                rTree.AddRecord(new Rectangle(FeatureDimension, point, point), name);
-            }
-
-            for (var i = 0; i < imageList.Length / 100; i++)
-            {
-                var name = imageList[i];
-                var category = name.Split('_')[0];
-                var feature = featureList[i];
-                var slices = feature.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-                var featureArr = (from slice in slices select float.Parse(slice)).ToList();
-                var point = new Point(FeatureDimension, featureArr);
-
-                var topResults = rTree.GetKNearestItems(point, TopK).ToList();
-                topResults.Remove(name);
-                var correctCount = 0;
-                topResults.ForEach(s =>
-                {
-                    var resultCategory = s.Split('_')[0];
-                    if (resultCategory == category)
-                        correctCount++;
-                });
-                Console.WriteLine(name + ' ' + (double) correctCount / (TopK - 1));
-            }
         }
     }
 }
